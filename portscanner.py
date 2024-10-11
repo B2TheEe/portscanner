64d9858a79260aef8bbe383505cc35c1d613eeab
Bente Schopman <schopmanbenteb2theee@gmail.com>
2024-10-11 19:32:32 +0200

    Creating class for GUI

--- portscanner.py
+++ portscanner.py
@@ -2,42 +2,49 @@ import socket
 import time
 from PyQt5.QtWidgets import *
 from ipaddress import ip_address
-
-def submit():
-    print("pressed")
-
-def gui():
-    app = QApplication([])
-    app.setStyle('Fusion')
-    window = QWidget()
-    layout = QVBoxLayout()
-    gui = QLabel('GUI')
-
-    ip_address = QLabel("IP Address")
-    ip_address_field = QLineEdit()
-    ports = QLabel("Ports")
-    ports_field = QLineEdit()
-
-    submit_button = QPushButton("submit")
-    submit_button.clicked.connect(submit)
-
-    layout.addWidget(gui)
-    layout.addWidget(ip_address)
-    layout.addWidget(ip_address_field)
-    layout.addWidget(ports)
-    layout.addWidget(ports_field)
-    layout.addWidget(submit_button)
-
-    gui.show()
-    ip_address.show()
-    ports.show()
+import sys
 
 
-    window.setLayout(layout)
-    window.show()
-    app.exec()
 
 
+class Window(QDialog):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.app = QApplication([])
+        self.app.setStyle('Fusion')
+
+        self.window = QWidget()
+        self.layout = QVBoxLayout()
+        self.gui = QLabel('GUI')
+
+        self.ip_address = QLabel("IP Address")
+        self.ip_address_field = QLineEdit()
+        self.ports = QLabel("Ports")
+        self.ports_field = QLineEdit()
+
+        self.submit_button = QPushButton("submit")
+        self.submit_button.clicked.connect(self.input)
+
+        self.layout.addWidget(self.gui)
+        self.layout.addWidget(self.ip_address)
+        self.layout.addWidget(self.ip_address_field)
+        self.layout.addWidget(self.ports)
+        self.layout.addWidget(self.ports_field)
+        self.layout.addWidget(self.submit_button)
+
+        self.gui.show()
+        self.ip_address.show()
+        self.ports.show()
+
+
+        self.window.setLayout(self.layout)
+        self.window.show()
+        self.app.exec()
+
+    def input(self,ip_address, ports):
+        print("pressed")
+        print(self.ip_address)
+        print(self.ports)
 
 
 
@@ -52,7 +59,17 @@ def scanner(ip,port):
         None
 
 def main():
-    gui()
+    # create pyqt5 app
+    app = QApplication(sys.argv)
+
+    # create the instance of our Window
+    window = Window()
+
+    # showing the window
+    window.show()
+
+    # start the app
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
