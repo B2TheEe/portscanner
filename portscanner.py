9ef1023f0afea42720e2e7f7b3214630636166c3
Bente Schopman <schopmanbenteb2theee@gmail.com>
2024-10-11 18:40:03 +0200

    Describing names at variables

--- portscanner.py
+++ portscanner.py
@@ -1,25 +1,35 @@
 import socket
 import time
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import *
 from ipaddress import ip_address
 
 
 def gui():
     app = QApplication([])
+    app.setStyle('Fusion')
     window = QWidget()
     layout = QVBoxLayout()
-    label = QLabel('GUI')
+    gui = QLabel('GUI')
 
-    label1 = QLabel("IP Address")
+    ip_address = QLabel("IP Address")
+    ip_address_field = QLineEdit()
+    ports = QLabel("Ports")
+    ports_field = QLineEdit()
+    submit_button = QPushButton("submit")
 
-    label2 = QLabel("Ports")
-    layout.addWidget(label)
-    layout.addWidget(label1)
-    layout.addWidget(label2)
 
-    label.show()
-    label1.show()
-    label2.show()
+    layout.addWidget(gui)
+    layout.addWidget(ip_address)
+    layout.addWidget(ip_address_field)
+    layout.addWidget(ports)
+    layout.addWidget(ports_field)
+    layout.addWidget(submit_button)
+
+    gui.show()
+    ip_address.show()
+    ports.show()
+
+
     window.setLayout(layout)
     window.show()
     app.exec()
